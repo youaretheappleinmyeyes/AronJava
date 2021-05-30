c755ec68d05f1bc8add4caeb6e960f32da4587fc
apple <admin@example.com>
2021-05-30 09:24:13 +0800

    aron —— Javastart

---
+++ test-java/src/test/java/com/example/testjava/TestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
